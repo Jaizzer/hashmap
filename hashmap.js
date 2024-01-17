4d68c687750401cf3d1e134a2dc928e013bb1f8e
Jaizzer <93913042+Jaizzer@users.noreply.github.com>
2024-01-18 07:07:16 +0800

    feat: Implement hashing function

--- hashmap.js
+++ hashmap.js
@@ -3,4 +3,15 @@ class HashMap {
         this.length = 0;
         this.buckets = new Array(16);
     }
+    // Hashing function
+    hash(string) {
+        let hashCode = 0;
+        const primeNumber = 31;
+
+        for (let i = 0; i < string.length; i++) {
+            hashCode = primeNumber * hashCode + string.charCodeAt(i);
+        }
+
+        return hashCode % this.buckets.length;
+    }
 }
